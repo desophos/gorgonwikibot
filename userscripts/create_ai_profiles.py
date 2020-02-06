f8dbb79fd76c808f404d5d86869e4244d9ea7fe7
desophos <1887450+desophos@users.noreply.github.com>
2020-02-06 12:00:23 -0600

    Add script for creating & uploading AI Profiles
    An AI Profile is a template containing the list of abilities associated
    with a given AI, used to simplify mob pages. Users can simply
    transclude the appropriate AI Profile instead of figuring out the
    abilities one by one.

---
+++ userscripts/create_ai_profiles.py
@@ -0,0 +1,99 @@
+import pywikibot
+from scripts.userscripts.GorgonWiki.RemoteData import Cdn
+
+
+def is_player_minigolem(name):
+    return "Minigolem" in name and "Enemy" not in name
+
+
+def validate_name(name, include=[]):
+    # no player pets or minigolems
+    return name in include or not ("Pet" in name or is_player_minigolem(name))
+
+
+def get_abilities(cdn):
+    include = [
+        "PetUndeadArrow1",
+        "PetUndeadArrow2",
+        "PetUndeadOmegaArrow",
+        "MinigolemBombToss4",
+        "MinigolemPunch4",
+        "MinigolemRageAcidToss4",
+    ]  # special cases for SkeletonDistanceArcher and SecurityGolem
+
+    return {
+        a["InternalName"]: {
+            "desc": a["Description"],
+            "icon": a["IconID"],
+            "keywords": a.get("Keywords", []),
+        }
+        for a in cdn.get_file("abilities").values()
+        if validate_name(a["InternalName"], include)
+        and "AttributesThatDeltaPowerCost" not in a  # no player abilities
+    }
+
+
+def get_ais(cdn):
+    return {
+        name: [
+            ability
+            for ability, params in v["Abilities"].items()
+            if "minLevel" not in params  # ignore scaled abilities
+        ]
+        for name, v in cdn.get_file("ai").items()
+        if validate_name(name)
+    }
+
+
+def generate_profiles(cdn):
+    """'''AIP:Kraken'''
+    : {{Combat Ability|KrakenBeak}}
+    : {{Combat Ability|KrakenSlam}}
+    : {{Combat Ability Rage|KrakenRage}}
+    <noinclude>[[Category:AI Profile]]</noinclude>
+    """
+    ais = get_ais(cdn)
+    abilities = get_abilities(cdn)
+    profiles = {}
+    for ai, alist in ais.items():
+        profile = ""
+        rages, nonrages = [], []
+        for a in alist:
+            if "RageAttack" in abilities[a]["keywords"]:
+                rages.append(a)
+            else:
+                nonrages.append(a)
+        # we want all nonrages before all rages
+        for a in nonrages:
+            profile += f": {{{{Combat Ability|{a}}}}}\n"
+        for a in rages:
+            profile += f": {{{{Combat Ability Rage|{a}}}}}\n"
+        profile += "<noinclude>[[Category:AI Profile]]</noinclude>"
+        profiles[ai] = profile
+    return profiles
+
+
+def main(*args):
+    cdn = Cdn()
+    local_args = pywikibot.handle_args(args)
+    dry_run = False
+
+    for arg in local_args:
+        option, sep, value = arg.partition(":")
+        if option == "-dry-run":
+            dry_run = True
+
+    site = pywikibot.Site()
+    for name, profile in generate_profiles(cdn).items():
+        page = pywikibot.Page(site, f"AIP:{name}")
+        page.text = profile
+        if dry_run:
+            pywikibot.output("Dry-run mode")
+            pywikibot.output(f"AIP:{name}\n{page.text}\n")
+        else:
+            page.save(summary="Create AI Profile page")
+            pywikibot.output(f"Page saved for profile {name}")
+
+
+if __name__ == "__main__":
+    main()
