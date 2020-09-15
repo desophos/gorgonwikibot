4f6ea5a81e265cd7b65122270bde601891a8ecb1
desophos <1887450+desophos@users.noreply.github.com>
2020-09-15 04:19:52 -0500

    Fix and simplify

--- scripts/generate_ability_templates.py
+++ scripts/generate_ability_templates.py
@@ -1,6 +1,6 @@
 import sys
 
-from scripts.create_ai_profiles import get_abilities
+from scripts.create_ai_profiles import get_abilities, is_valid_enemy_ability
 
 
 def write_template(abilities, key, filename):
@@ -8,11 +8,7 @@ def write_template(abilities, key, filename):
         f.writelines(f"| {k}={v[key]}\n" for k, v in abilities)
 
 
-def main():
-    abilities = get_abilities().items()
+if __name__ == "__main__":
+    abilities = get_abilities(is_valid_enemy_ability).items()
     write_template(abilities, "Description", "abilities.txt")
     write_template(abilities, "IconID", "icons.txt")
-
-
-if __name__ == "__main__":
-    main(sys.argv)
