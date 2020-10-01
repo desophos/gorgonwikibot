9e122a39e288430bb948bac491f86ecf4c2df2e0
desophos <1887450+desophos@users.noreply.github.com>
2020-10-01 00:07:35 -0500

    Some tests for ability pages

---
+++ tests/test_create_ability_pages.py
@@ -0,0 +1,66 @@
+from functools import reduce
+from operator import not_, truth
+
+import pytest
+from scripts.create_ability_pages import *
+from tests import dummy_ability
+
+
+@pytest.mark.parametrize(
+    "btest,data",
+    [
+        (not_, {"Keywords": ["Lint_NotLearnable"]}),
+        (not_, {"Keywords": ["_", "Lint_NotLearnable", "test"]}),
+        (truth, {"Keywords": ["_", "test"]}),
+        (truth, {}),
+    ],
+)
+def test_is_learnable(btest, data):
+    assert btest(is_learnable(dummy_ability(data)))
+
+
+def test_ability_chains():
+    chains = ability_chains()
+    # check sorting
+    for basename, chain in chains.items():
+        assert reduce(lambda a, b: a.name <= b.name and b, chain), basename
+    # special cases
+    for name in ["(Self Bomb)", "Self Bomb", "Charm Rat"]:
+        assert name not in chains
+    for name in ["Punch", "Sword Slash"]:
+        assert name in chains
+    for k, v in disambiguate.items():
+        assert v in chains and k not in chains
+    # duplicates
+    dupe_skills = {"Front Kick": ["Unarmed", "Cow"]}
+    dupe_names = []
+    for name, skills in dupe_skills.items():
+        assert name not in chains
+        for skill in skills:
+            fullname = f"{name} ({skill})"
+            assert fullname in chains
+            for a in chains[fullname]:
+                assert a.data["Skill"] == skill
+            dupe_names.append(fullname)
+    # make sure keys match values
+    for basename, chain in chains.items():
+        # exclude special cases
+        assert basename in dupe_names + list(disambiguate.values()) or all(
+            basename in a.name for a in chain
+        )
+
+
+def test_generate_infobox():
+    raise NotImplementedError
+
+
+def test_generate_infoboxes():
+    raise NotImplementedError
+
+
+def test_generate_page():
+    raise NotImplementedError
+
+
+def test_generate_pages():
+    raise NotImplementedError
