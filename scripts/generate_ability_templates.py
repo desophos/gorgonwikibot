a31d0683c8d45ce357e96141cb03ad088a2ca90f
desophos <1887450+desophos@users.noreply.github.com>
2021-04-19 02:56:34 -0500

    Include abilities without descriptions
    Use "(No Description)" as placeholder

--- scripts/generate_ability_templates.py
+++ scripts/generate_ability_templates.py
@@ -5,14 +5,19 @@ from scripts.create_ai_profiles import get_abilities
 
 def write_template(abilities, key, filename):
     with open(filename, "w") as f:
-        f.writelines(f"| {k}={v[key]}\n" for k, v in abilities)
+        f.writelines(f"| {k}={v[key]}\n" for k, v in abilities.items())
 
 
 if __name__ == "__main__":
 
     def validator(a):
-        return not a.is_player and a.data["Description"]
+        return not a.is_player
 
-    abilities = get_abilities(validator).items()
+    abilities = {}
+    for k, v in get_abilities(validator).items():
+        if not v["Description"]:
+            v["Description"] = "(No Description)"
+        abilities[k] = v
+
     write_template(abilities, "Description", "logs/abilities.txt")
     write_template(abilities, "IconID", "logs/icons.txt")
