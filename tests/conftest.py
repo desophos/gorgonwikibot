fc297624546ca6ee695c920958d6cfa6ac96a575
desophos <1887450+desophos@users.noreply.github.com>
2021-04-19 01:58:35 -0500

    Add dummy Ai fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from gorgonwikibot.content import Ability
+from gorgonwikibot.content import Ability, Ai
 
 
 @pytest.fixture
@@ -8,5 +8,15 @@ def dummy_ability():
         data = {"Name": "_", "InternalName": "_", "Description": "_"}
         data.update(custom or {})
         return Ability("_", data)
+
+    return _dummy
+
+
+@pytest.fixture
+def dummy_ai():
+    def _dummy(id=None, custom=None):
+        data = {"Abilities": {}}
+        data.update(custom or {})
+        return Ai(id or "_", data)
 
     return _dummy
